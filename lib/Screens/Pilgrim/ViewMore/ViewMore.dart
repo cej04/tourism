7833bfb5051f6ff1b5af236c1fc515e746b1b317
cej04 <githubcej@gmail.com>
2022-06-27 13:01:39 +0530

    structured pilgrims folder

---
+++ lib/Screens/Pilgrim/ViewMore/ViewMore.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:ktmtourism/Screens/Pilgrim/ViewMore/View.dart';
+import 'package:ktmtourism/Screens/Pilgrim/ViewMore/ViewMorePigrimCard.dart';
+import 'package:ktmtourism/Screens/Widget/appbarWidget.dart';
+
+class ViewMorePilgrim extends StatelessWidget {
+  const ViewMorePilgrim({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: const MyAppBar(),
+      ),
+      body: ListView.builder(
+          itemCount: pilgrimviewmore.length,
+          itemBuilder: (context, index) =>
+              ViewMoreCard(pilgrimviewmore: pilgrimviewmore[index], press: () {}
+                  
+                  )),
+    );
+  }
+}
