94ae5572e77419ea936347390a54cac7d8d0c7cb
ADIO <royjosphp@gmail.com>
2022-06-27 15:29:25 +0530

    pilgrims data updated

--- lib/Screens/Pilgrim/pilgrims.dart
+++ lib/Screens/Pilgrim/pilgrims.dart
@@ -1,10 +1,12 @@
 class Pilgrims {
-  final String image, title, description;
+  final String image, title, subtitle, distance, description;
   final double lat, long;
 
   Pilgrims(
       {required this.image,
       required this.title,
+      required this.subtitle,
+      required this.distance,
       required this.description,
       required this.lat,
       required this.long});
@@ -14,48 +16,62 @@ List<Pilgrims> pilgrims = [
   Pilgrims(
       image: "assets/images/juma.jpg",
       title: "Thazhathangadi Juma Masjid",
+      subtitle: "One of the oldest mosques in India",
+      distance: "2.5 km\nfrom Kottayam",
       description:
-          "The Thazhathangadi Juma Masjid is one of the oldest mosques in India (about 1,000 years old). It is believed that Habib Dinar (son of Malik Dinar who introduced Kerala to Islam) constructed the masjid. It is built in the traditional Kerala style of architecture. It is famous for its richness of architecture, wood caring and the beauty. This mosque is situated on the bank of Meenachil river.",
+          "The Thazhathangadi Juma Masjid is one of the oldest mosques in India (about 1,000 years old). It is believed that Habib Dinar (son of Malik Dinar who introduced Kerala to Islam) constructed the masjid. It is built in the traditional Kerala style of architecture. It is famous for its richness of architecture, wood caring and the beauty. This mosque is situated on the banks of the Meenachil river.\n\nThazhathangady Juma Masjid is also called as the 'Taj Juma Masjid'. The ancestors of this Masjid came and settled in Kottayam, from different parts of Kerala. The Muslims who lived here played an active role in Freedom Struggle and other National Movements.",
       lat: 9.59862752669,
       long: 76.5055463601),
   Pilgrims(
       image: "assets/images/manarcadchurch.jpg",
       title: "St. Mary’s Church, Manarcad",
+      subtitle: "St. Mary's Jacobite Syrian Orthodox Cathedral, Manarcad ",
+      distance: "9 km\nfrom Kottayam",
       description:
-          "Located 8 km away from Kottayam town, the St. Mary’s Church at Manarcaud is one of the most important churches of the Malankara Jacobite Syrian Christians. The ettunompu (8-day fasting) which is an annual event held between September 1st and 8th of every year sees devotees from far and wide thronging the church to participate in the prayers and rituals.",
+          "Located 9 km away from Kottayam town, the St. Mary’s Church at Manarcaud is one of the most important churches of the Malankara Jacobite Syrian Christians. The ettunompu (8-day fasting) which is an annual event held between September 1st and 8th of every year sees devotees from far and wide thronging the church to participate in the prayers and rituals.\n\nThe church architecture resembles that of the Orvieto Cathedral. The church administers various spiritual organisations, schools and a hospital.",
       lat: 9.59988435491,
       long: 76.5853019716),
   Pilgrims(
       image: "assets/images/mahadevartemple.jpg",
       title: "Thirunakkara Mahadevar Temple",
+      subtitle: "About 500 years old Temple that was built by the Thekkumkoor Raja",
+      distance: "1 km\nfrom Kottayam",
       description:
-          "Thirunakkara Mahadevar Temple situated in the heart of Kottayam city is one of the 108 revered Shivalayas in central Kerala. The temple is about 500 years old and was built by the Thekkumkoor raja. It also preserves a number of unique sculptures and murals of various Hindu deities. A common belief is that the idol of Shiva here is installed by sage Parashurama himself.",
+          "Thirunakkara Mahadevar Temple situated in the heart of Kottayam city is one of the 108 revered Shivalayas in central Kerala. The temple is about 500 years old and was built by the Thekkumkoor raja. It also preserves a number of unique sculptures and murals of various Hindu deities. A common belief is that the idol of Shiva here is installed by sage Parashurama himself.\n\nThe whole temple complex occupies about 4 acres (1.6 ha) of land which is rich in many trees and plants. One of the largest Koothambalams in Kerala is in this temple situated in the southeast corner. The temple has shrines for various sub deities in different locations. The southeast entrance consists of the shrines of lord Ayyappa and Ganesha. Naga pratishtas are also close to these shrines. In the northwest section, there is a Chethi (Jungle flame) flower, commonly seen in many Hindu shrines across Kerala. ",
       lat: 9.590549,
       long: 76.519114),
   Pilgrims(
       image: "assets/images/erumelimasjid.jpg",
       title: "Vavarupalli, Erumeli Juma Masjid",
+      subtitle: "The mosque where Hindu pilgrims customarily worship before their trek to Sabarimala.",
+      distance: "50 km\nfrom Kottayam",
       description:
-          "Located at about 49 km off Kottayam is the Vavarambalam at Erumeli which is a mosque dedicated to Vavar, a companion of the deity Lord Ayyappa of the Sabarimala Temple. The mosque enjoys a unique status in that the Hindu pilgrims pay their respects to Vavar at the Vavarambalam before embarking on their trek to Sabarimala. The festival here is the Erumeli Pettathullal held in the month of January.",
+          "Located at about 50 km from Kottayam is the Vavarambalam at Erumeli which is a mosque dedicated to Vavar, a companion of the deity Lord Ayyappa of the Sabarimala Temple. The mosque enjoys a unique status in that the Hindu pilgrims pay their respects to Vavar at the Vavarambalam before embarking on their trek to Sabarimala. The festival here is the Erumeli Pettathullal held in the month of January.\n\nThe mosque has an imposing structure which large domes and a number of minarets. The mosque is visited by a lot of tourists for its architectural magnificence and simplicity. The mosque has a Muslim saint for performing the daily rites, since Vavar followed the religion of Islam. The mosque doesn’t have a statue, but has a stone slab with inscriptions on it, just like how other masjids have. The walls are covered with green silk cloth and there is also a sword which belonged to Vavar, symbolising his valour.",
       lat: 9.45709237535,
       long: 76.857055361),
   Pilgrims(
       image: "assets/images/ettumanoortemple.jpg",
       title: "Ettumanoor Mahadeva Temple",
+      subtitle: "One of the major Shiva temples in Kerala",
+      distance: "11 km\nfrom Kottayam",
       description:
-          "Ettumanoor Mahadeva temple is an ancient Shiva temple in Kottayam, Kerala, India. It has brought glory and fame to the place. Myths have it that the Pandavas and the sage Vyasa had worshipped at this temple. The name of the place has its origin from the word ‘manoor’, which means the land of deer.",
+          "Ettumanoor Mahadeva temple is an ancient Shiva temple in Kottayam, Kerala, India. It has brought glory and fame to the place. Myths have it that the Pandavas and the sage Vyasa had worshipped at this temple. The name of the place has its origin from the word ‘manoor’, which means the land of deer.\n\nEttumanoor Mahadeva Temple hosts the arattu festival, celebrated on a grand scale on the Thiruvathira day in February–March each year. Many people come to the temple on the 8th and 10th day of the festival, when seven and a half elephants (in Malayalam: ezharaponnaana) made of gold (nearly 13 Kgms) will be held in public view. This statue was donated to the temple by a travancore maharaja. The temple, one of the wealthiest Devaswoms in Kerala, has many valuable possessions.",
       lat: 9.673678,
       long: 76.56077),
   Pilgrims(
       image: "assets/images/mannanamchurch.jpg",
       title: "St. Joseph’s Monastery, Mannanam",
+      subtitle: "An important pilgrimage center for followers of Saint Kuriakose Elias Chavara.",
+      distance: "8 km\nfrom Kottayam",
       description:
-          "Built by the blessed St. Chavara Kuriakose Elias, the St. Joseph’s Monastery at Mannanam is located 10 km away from Kottayam. The mortal remains of this saintly priest are preserved here. Thousands of devotees congregate here to pay homage to the saint.",
+          "Built by the blessed St. Chavara Kuriakose Elias in 1837, the St. Joseph’s Monastery at Mannanam is located 8 km away from Kottayam. The mortal remains of this saintly priest are preserved here. Thousands of devotees congregate here to pay homage to the saint.\n\nSt. Kuriakose also started the first printing press in Kottayam in the church complex in 1846. The printing press was used to bring out the first Malayalam daily Nasrani Deepika. When St. Kuriakose died in 1871, he was buried at St. Philomina's Church, Koonammavu. His mortal remains were later transferred to the St. Joseph Church in 1889.",
       lat: 9.64616,
       long: 76.52007),
   Pilgrims(
       image: "assets/images/bhmchurch.jpg",
       title: "St. Alphonsa Shrine Church, Bharananganam",
+      subtitle: "The mortal remains of the St. Alphonsa is entombed at this church",
+      distance: "33 km\nfrom Kottayam",
       description:
           "The main structure of the church, which began to be known as St.Alphonsa Shrine Church following the canonization of Sr. Alphonsa on 12th October 2008 had been built in the year 1945 as a cemetery chapel of St. Mary’s Church Bharananganam. Sr.Alphonsa is the first nun ever to have been buried there. Her tomb now stands at the very place where her mortal remains were buried on 29 July 1946.\n\nDuring the period 1963-64, a side wing with family tombs was added. With the completion of new tombs in the parish graveyard in 1985, burial in the cemetery chapel was discontinued and the mortal remains of the dead interred therein except those of St.Alphonsa were exhumed and inhumed in the newly built vaults/chambers.",
       lat: 9.7026185,
@@ -63,8 +79,10 @@ List<Pilgrims> pilgrims = [
   Pilgrims(
       image: "assets/images/mookambikatemple.jpg",
       title: "Panachikkadu Dakshina Mookambika Temple",
+      subtitle: "A Hindu temple dedicated to the goddess Saraswati.",
+      distance: "11 km\nfrom Kottayam",
       description:
-          "The Panachikkadu Temple, also known as the Dakshina Mookambika (Mookambika of the South) Temple, is a Hindu temple dedicated to the goddess Saraswati. The temple is located in the southern region of the Indian Peninsula, in Panachikkad in Kottayam, Kerala, India.",
+          "The Panachikkadu Temple, also known as the Dakshina Mookambika (Mookambika of the South) Temple, is a Hindu temple dedicated to the goddess Saraswati. The temple is located in the southern region of the Indian Peninsula, in Panachikkad in Kottayam, Kerala, India.\n\nIt is one of the most prominent Saraswati temples for devotees in Kerala. But, the main deity of the temple is Lord Vishnu, who was installed long before Goddess Saraswati. Still the temple is known as Saraswati temple, due to the popularity of the Goddess. There are sub-shrines for Lord Shiva, Lord Ganapathi, Lord Ayyappan, Snake deities and Panachikkattu Yakshi inside the temple",
       lat: 9.513989,
       long: 76.538704),
 ];
