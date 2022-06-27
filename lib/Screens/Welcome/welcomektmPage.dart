d72cbb2ac8d8131d315ad396977b116da599e5e0
cej04 <githubcej@gmail.com>
2022-06-27 15:37:19 +0530

    added padding and text style

--- lib/Screens/Welcome/welcomektmPage.dart
+++ lib/Screens/Welcome/welcomektmPage.dart
@@ -248,32 +248,44 @@ class _welcomektmPageState extends State<welcomektmPage> {
                   itemCount: welcome.length,
                   itemBuilder: (context, index) {
                     return Padding(
-                        padding: const EdgeInsets.all(25.0),
+                        padding: const EdgeInsets.only(left:25.0,right: 25.0,top: 25.0),
                         child: GestureDetector(
                           child: 
-                        Card(
-                        color: Colors.grey[200],
-                        clipBehavior: Clip.antiAlias,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Column(
-                          children: [
-                            Image.asset(welcome[index].image),
-                            ListTile(
-                              //   leading: Icon(Icons.arrow_circle_right),
-                              title: Text(welcome[index].title),
-                              subtitle: Text(
-                                welcome[index].subtitle1,
-                              //  "Thi sis a sample subtitle regarding th etitle",
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6)),
+                          Card(
+                          color: Colors.grey[200],
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            children: [
+                              Image.asset(welcome[index].image),
+                              ListTile(
+                           
+                                title: Text(welcome[index].title  ,style: Theme.of(context).textTheme.headline6?.copyWith(
+                      color: Colors.black, fontWeight: FontWeight.bold)),
+                      
+                      
+                                subtitle: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                  
+                                    welcome[index].subtitle1,
+                                   textAlign: TextAlign.justify,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText2
+                                ?.apply(
+                                  color: Colors.black,
+                                )
+                                .copyWith(height: 1.2),
+                                  ),
+                                ),
+                                trailing: Icon(Icons.navigate_next),
                               ),
-                              trailing: Icon(Icons.navigate_next),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
                           
                           onTap: () => Navigator.push(
                               context,
