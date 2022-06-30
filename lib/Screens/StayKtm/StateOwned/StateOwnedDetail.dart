2e1eaeee0cea2dbcb47b268ae38d0f176a28d17d
ADIO <royjosphp@gmail.com>
2022-06-30 12:29:04 +0530

    call and email facility in stay state owned

--- lib/Screens/StayKtm/StateOwned/StateOwnedDetail.dart
+++ lib/Screens/StayKtm/StateOwned/StateOwnedDetail.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ktmtourism/Screens/StayKtm/StateOwned/StateOwned.dart';
 import 'package:ktmtourism/Screens/Widget/appbarWidget.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../../Utils/constants.dart';
 import 'package:ktmtourism/Screens/map_utils.dart';
-
 
 class StateOwnedDetailsPage extends StatelessWidget {
   final StateOwned stateowned;
@@ -20,13 +20,13 @@ class StateOwnedDetailsPage extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: [
- 
               Padding(
                 padding: const EdgeInsets.all(kDefaultPadding),
                 child: Container(
-                  child: Text(stateowned.title,
-                      style: Theme.of(context).textTheme.headline6?.copyWith(
-                          color: Colors.black, fontWeight: FontWeight.bold)),
+                  child: Text(stateowned.title,style: Theme.of(context).textTheme.headline6),
+                      // style: Theme.of(context).textTheme.headline6?.copyWith(
+                      //     color: Colors.black, fontWeight: FontWeight.bold)
+                      // ),
                 ),
               ),
               Container(
@@ -38,81 +38,68 @@ class StateOwnedDetailsPage extends StatelessWidget {
                       blurRadius: 6.0,
                     ),
                   ],
-               
                   image: DecorationImage(
                     image: AssetImage(stateowned.image),
                     fit: BoxFit.fill,
-
-                  
                   ),
                 ),
               ),
-              GestureDetector(
-            onTap: () {
-              MapUtils.openMap(stateowned.latitude, stateowned.longitude);
-            },
-            child: Container(
-
-                
-                height: MediaQuery.of(context).size.height / 14.5,
-              
-                child: Image.asset(
-                  'assets/images/location1.png',
-                  fit: BoxFit.fill,
-                )),
-          ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
                   children: [
-                    Card(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          ListTile(
-                            leading: Icon(
-                              Icons.location_on,
-                              color: Colors.red,
-                            ),
-                            subtitle: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(stateowned.address,
-                                  style: TextStyle(color: Colors.blue)),
-                            ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          title: Text(stateowned.address),
+                          leading: ElevatedButton.icon(
+                            icon: Icon(Icons.location_pin),
+                            label: Text('Locate on Map'),
+                            onPressed: () {
+                              MapUtils.openMap(
+                                  stateowned.latitude, stateowned.longitude);
+                            },
                           ),
-                          ListTile(
-                            leading: Icon(
-                              Icons.local_phone_rounded,
-                              color: Colors.grey,
-                            ),
-                          
-                            subtitle: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(stateowned.phone.toString(),
-                                  style: TextStyle(color: Colors.blue)),
-                            ),
-                          ),
-                          ListTile(
-                            leading: Icon(
-                              Icons.mail_outline_rounded,
-                              color: Colors.green,
-                            ),
-                            
-                            subtitle: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(stateowned.email.toString(),
-                                  style: TextStyle(color: Colors.blue)),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                        ),
 
-                  
+                        ListTile(
+                          title: Text(
+                            stateowned.phone.toString(),
+                          ),
+                          leading: ElevatedButton.icon(
+                            icon: Icon(Icons.call),
+                            label: Text('Make a call'),
+                            onPressed: () async {
+                              final phoneNumber = stateowned.phone;
+                              final url = 'tel:$phoneNumber';
+                              // final url = 'tel:$stateowned.phone';
+                              if (await canLaunch(url)) {
+                                await launch(url);
+                              }
+                            },
+                          ),
+                        ),
+
+                        ListTile(
+                          title: Text(stateowned.email.toString()),
+                          leading: ElevatedButton.icon(
+                            icon: Icon(Icons.email),
+                            label: Text('Send an email'),
+                            onPressed: () async {
+                              final url = 'mailto:$stateowned.email';
+                              if (await canLaunch(url)) {
+                                await launch(url);
+                              }
+                            },
+                          ),
+                        ),
+                        // ),
+                      ],
+                    ),
                   ],
                 ),
               )
-
             ],
           ),
         ));
