7a9f1f4e70e05aa13af26edcbae8ca376660098d
ADIO <royjosphp@gmail.com>
2022-07-05 14:09:28 +0530

    stay images added as new folder stay in assets

---
+++ lib/Screens/Ayurveda.dart
@@ -0,0 +1,55 @@
+class Ayurveda {
+  final String name,
+      image,
+      classification,
+      address,
+      ph1,
+      ph2,
+      ph3,
+      email1,
+      email2,
+      website,
+      overview,
+      facilities,
+	 treatment,
+      latitude,
+      longitude;
+
+  Ayurveda({
+    required this.name,
+    required this.image,
+    required this.classification,
+    required this.address,
+    required this.ph1,
+    this.ph2 = "",
+    this.ph3 = "",
+    required this.email1,
+    this.email2 = "",
+    this.website = "",
+    required this.overview,
+    required this.facilities,
+    this.treatment = "",
+    required this.latitude = "",
+    required this.longitude = "",
+  });
+}
+
+List<Ayurveda> ayurveda = [
+  Ayurveda(
+    name: "",
+    image: "",
+    classification: "",
+    address: "",
+    ph1: "",
+    ph2: "",
+    ph3: "",
+    email1: "",
+    email2: "",
+    website: "",
+    overview: "",
+    facilities: "",
+    treatment: "",
+    latitude: "",
+    longitude: "",
+  ),
+];
