15557ec5b12ad459e3b8ba402a9ca3e72c0a3f0d
cej04 <githubcej@gmail.com>
2022-07-20 16:07:56 +0530

    police page

---
+++ lib/Screens/PrivateHospitals/Privatehospital.dart
@@ -0,0 +1,45 @@
+class PrivateHospital{
+String name,ph1;
+
+  
+
+
+PrivateHospital({
+required this.name,
+required this.ph1,
+
+});
+}
+
+List<PrivateHospital> privatehospital =
+ [{"sl":1,"name":"Alphonsa hospital, Changanassery","ph1":"+914812420103"},
+ {"sl":2,"name":"Amala Medical Centre, Kottayam","ph1":"+919495264479"},
+ {"sl":3,"name":"Amans Carewell Hospital,Erattupetta","ph1":"+914822274573"},
+ {"sl":4,"name":"Anugraha Hospital, Karapuzha","ph1":"+914812583060"},
+ {"sl":5,"name":"Aradhana Hospital,Vaikom","ph1":"+914829275583"},
+ {"sl":6,"name":"Arunodya Puthupally","ph1":"+914812352207"},
+ {"sl":7,"name":"Asha Hospital Veloor","ph1":"+914812373304"},
+ {"sl":8,"name":"Hospital, Melukavumattom","ph1":"+914822219036"},
+ {"sl":9,"name":"Athira Nursing Home ,Mukoottuthara","ph1":"+914828254945"},
+ {"sl":10,"name":"Ayurgram Hospital Vaikom","ph1":"+914829326875"},
+ {"sl":11,"name":"Ayurlakshmi Kumarakom","ph1":"+914812523313"},
+ {"sl":12,"name":"B.J Clinic,Erattupetta","ph1":"+914822273045"},
+ {"sl":13,"name":"Base hospital ,Manarkadu","ph1":"+914812371213"},
+ {"sl":14,"name":"Bhavana Hospital ,Athirampuzha","ph1":"+919495445725"},
+ {"sl":15,"name":"Bhavana Children Hospital,Kottayam","ph1":"+914812552920"},
+ {"sl":16,"name":"CMM Hospital ,Karukachal","ph1":"+914812485127"},
+ {"sl":17,"name":"C . N Hospital,Changanacharry","ph1":"+914812722888"},
+ {"sl":18,"name":"Cardinal Speciality Hospital,Monipally","ph1":"+914822243099"},
+ {"sl":19,"name":"Carewell Hospital,Kallara","ph1":"+914829267473"},
+ {"sl":20,"name":"Carithas Hospital ,Thellokom","ph1":"+914812790025"},
+ {"sl":21,"name":"Cherupushpam Hospital ,Mukoottutha","ph1":"+914828254980"},
+ {"sl":22,"name":"Cherupushpam Hospital,Pala","ph1":"+914822213309"},
+ {"sl":23,"name":"Chiithra Hospital,Ayarkunnam","ph1":"+914812542270"},
+ {"sl":24,"name":"Chiithra Hospital,Manimala","ph1":"+914828247454"},
+ {"sl":25,"name":"Christ Raj Hospital,Pala","ph1":"+914822212760"},
+ {"sl":26,"name":"CKC Smaraka, Erattupetta","ph1":"+919947804854"},
+ {"sl":27,"name":"CMC Hospital,Kottayam","ph1":"+914812597177"},
+ {"sl":28,"name":"Co Operative Hospital Kaduthuruthy","ph1":"+914829282574"},
+ {"sl":29,"name":"Devi Hospital,Mundakkayam","ph1":"+914828280545"},{"sl":30,"name":"Devine Hospital,Kuruppamthara","ph1":"+914829242897"},{"sl":31,"name":"Elite Hospital,Chingavanam","ph1":"+914816524371"},{"sl":32,"name":"Fathima Mission Hospital,Kozhuvanal","ph1":"+914822267027"},{"sl":33,"name":"Goods Samarian Hospital,Ponkunnam","ph1":"+914828221218"},{"sl":34,"name":"Goutham Hospital Thaiayolaparampu","ph1":"+914829237749"},{"sl":35,"name":"Grace Hospital ,Changanacherry","ph1":"+914812421914"},{"sl":36,"name":"Grace Medical Centre,Aimanam","ph1":"+914812517676"},{"sl":37,"name":"HNL Hospital ,Perunna","ph1":"+914829256211"},{"sl":38,"name":"HDP Hospital,Kottayam","ph1":"+914812711418"},{"sl":39,"name":"High Range Hospital,Erumely","ph1":"+914828212927"},{"sl":40,"name":"High Range Hospital,Parathodu","ph1":"+914828270578"},{"sl":41,"name":"Holly Cross,Koovapally","ph1":"+914828251164"},{"sl":42,"name":"Holly Family Hospitals,Bharanaganam","ph1":"+914822277329"},{"sl":43,"name":"Holy Ghost Hospital,Muttichira","ph1":"+914829282224"},{"sl":44,"name":"Holy Sprit Hospital Thangana","ph1":"+914812720813"},{"sl":45,"name":"Hosanna mount Hospital,Poovathodu","ph1":"+914822236226"},{"sl":46,"name":"I.H.M .Hospital, Bharanaganam","ph1":"+914822236228"},{"sl":47,"name":"Indo American brain &spinal Centre ,Vaikom","ph1":"+914829273281"},{"sl":48,"name":"Jacob Memorial Hospital ,Kottayam","ph1":"+914812432213"},{"sl":49,"name":"Janatha Hospital,Pala","ph1":"+914822212132"},{"sl":50,"name":"Jaya nursing home,Thalayolaparambu","ph1":"+914829238188"},{"sl":51,"name":"Jijo Hospital,Poonjar","ph1":"+914822272530"},{"sl":52,"name":"K.V.M Hospital,Ponnkunnam","ph1":"+914828221713"},{"sl":53,"name":"Kadampuzha Hospital,Kanjirapally","ph1":"+914828202832"},{"sl":54,"name":"KM Hospital ,Nedumkunnam","ph1":"+914812415281"},{"sl":55,"name":"M.G.D.M Hospital, Kanghazha","ph1":"+914812494478"},{"sl":56,"name":"Little Lurde Hospital,Kidangoor","ph1":"+914822254156"},{"sl":57,"name":"MAM Hospital,Kajirally","ph1":"+914828329235"},{"sl":58,"name":"Mandiram Hospital,Manganam","ph1":"+914812578393"},{"sl":59,"name":"Manimala Medical centre ,Manimala","ph1":"+914828247021"},{"sl":60,"name":"Mar Dionysius Hospital, Kottayam","ph1":"+914812566588"},{"sl":61,"name":"Mariyan Clinic,Hospital","ph1":"+919497667353"},{"sl":62,"name":"Mariyan Hospital, Changanassrery","ph1":"+914812422017"},{"sl":63,"name":"Mariyan Medical centre Arunapuram","ph1":"+914822214219"},{"sl":64,"name":"Mary Matha Hospital,Kajirapally","ph1":"+914828201015"},{"sl":65,"name":"Mary Qeen Hospital,Kanjirapally","ph1":"+914828202460"},{"sl":66,"name":"Mary Qeen’s Mission Hospital Kanjirapally ,","ph1":"+914828201300"},{"sl":67,"name":"Matha Hospital,Thellakom","ph1":"+914812790920"},{"sl":68,"name":"Medical centre,Manimala","ph1":"+914828247027"},{"sl":69,"name":"Medical centre,Kottayam","ph1":"+914812562860"},{"sl":70,"name":"Medical Mission Hospital,Vazhoor","ph1":"+914812456322"},{"sl":71,"name":"Medicare Children Hospital,Erattupetta","ph1":"+914822272485"},{"sl":72,"name":"Medicare Hospital, Kottayam","ph1":"+914812516377"},{"sl":73,"name":"Mercy Hospital, Chengalam","ph1":"+914812704226"},{"sl":74,"name":"Mercy Hospital,Thalayolaparampu","ph1":"+914829236131"},{"sl":75,"name":"Mercy Nursing Home ,Karukachal","ph1":"+914812485135"},{"sl":76,"name":"Mercy Nursing Home, Thengana","ph1":"+914812474070"},{"sl":77,"name":"Mithranikethan Hospital ,Vagamon","ph1":"+914822289305"},{"sl":78,"name":"MMT Hospital, Mundakayam","ph1":"+914828320800"},{"sl":79,"name":"Mother Mary Hospita,Mundakayam","ph1":"+914828274667"},{"sl":80,"name":"N.S.S Hospital, Kummannoor","ph1":"+914822255104"},{"sl":81,"name":"N.S.SMedical mission Hospital, Changanassrery","ph1":"+914812420018"},{"sl":82,"name":"N.S.SMedical mission Hospital,Karukachal","ph1":"+914812485222"},{"sl":83,"name":"Paret Mar Evanious,Hospital,Puthupally","ph1":"+914812352873"},{"sl":84,"name":"People Hospital,Mundakayam","ph1":"+914828272526"},{"sl":85,"name":"Salvation Army Hospital Medical Hospital, Parathodu","ph1":"+914813494607"},{"sl":86,"name":"Santo Hospital ,Vaikom","ph1":"+914829 275109"},{"sl":87,"name":"St.Augustines Hospital,Ramapuram","ph1":"+914822260367"},{"sl":88,"name":"St.George Hospital,Menvettom","ph1":"+914829249103"},{"sl":89,"name":"St.George Mount Hospital,Vayala","ph1":"+914822228246"},{"sl":90,"name":"ST.Mary’s Hospital,Poovarani","ph1":"+914822225294"},{"sl":91,"name":"ST.Thomas Hospital ,Chethipuzha","ph1":"+914812721797"},{"sl":92,"name":"ST.Thomas Hospital,Vadayar","ph1":"+914829236140"},{"sl":93,"name":"ST.Vincent Hospital,Kuravilangadu","ph1":"+914822231636"},{"sl":94,"name":"ST.Thomas Hospital,Kuruppumthara","ph1":"+914829242726"},{"sl":95,"name":"Susrusha Hospital,Kottayam","ph1":"+914812391826"},{"sl":96,"name":"Thekkedathu Medical Centre , Kuravilangadu","ph1":"+914822230421"},{"sl":97,"name":"Thengana Medical Mission Hospital","ph1":"+914812473237"},{"sl":98,"name":"Udayagriy Hospital,Chananassery","ph1":"+914812421250"},{"sl":99,"name":"Vaikom Sathyagraja Memorial Hospital,Vaikom","ph1":"+914829221701"},{"sl":100,"name":"VMC Hospital,Kaduthuruthy","ph1":"+919388627471"},
+ {"sl":101,"name":"VSM Hospital,Vaikom","ph1":"+914829221701"}]
+;
